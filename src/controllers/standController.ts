713741fe8ef184d4924e29cc2700d83377eb3768
Guruprasanna02 <guruprasannavasudevan02.com>
2021-12-07 21:04:11 +0530

    feat: add register cycles,docks,stands

--- src/controllers/standController.ts
+++ src/controllers/standController.ts
@@ -34,7 +34,7 @@ export const registerStand = async (
     });
     if (exists?.Id && exists.Id) {
       res.send(
-        '<html><head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"><div class="alert alert-danger" role="alert">A stand in this location already exists</div><a href="/register/dock">Return back</a></head></html>'
+        '<html><head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"><div class="alert alert-danger" role="alert">A stand in this location already exists</div><a href="/register/stand">Return back</a></head></html>'
       );
       return res.status(409);
     }
@@ -49,6 +49,7 @@ export const registerStand = async (
     res.send(
       '<html><head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"><div class="alert alert-success" role="alert" style="text-align:center;">Regsitration of Stand was successful!</div><a href="/register/stand">Return back</a></head></html>'
     );
+    // res.redirect('/');
     return res.status(200);
   } catch (error) {
     return res.status(500).json({ message: 'Internal server error' });
